cce082fceea5f896815f04e05ec6fdb33831b620
Atul Dubey <atuldubey017@gmail.com>
2022-07-25 15:39:33 +0530

    insertion sort

--- src/main/java/com/learndsfinal/dsalgo/arrays/ArraysImplementation.java
+++ src/main/java/com/learndsfinal/dsalgo/arrays/ArraysImplementation.java
@@ -13,108 +13,103 @@ import java.util.Arrays;
 public class ArraysImplementation {
     private int[] arr;
     private int length;
-    public ArraysImplementation(int length)
-    {
-        this.arr=new int[length];
-        this.length=length;
+
+    public ArraysImplementation(int length) {
+        this.arr = new int[length];
+        this.length = length;
     }
-    public void setArray(int pass[])
-    {
+
+    public void setArray(int pass[]) {
         if (this.length > pass.length) {
             return;
         }
-        for(int i=0;i<pass.length;i++)
-        {
-            this.arr[i]=pass[i];
+        for (int i = 0; i < pass.length; i++) {
+            this.arr[i] = pass[i];
         }
     }
-    public int [] getAllWithoutEvenNumber()
-    {
-        int oddCount=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            oddCount+=this.arr[i]%2 != 0 ? 1 : 0;
+
+    public int[] getAllWithoutEvenNumber() {
+        int oddCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            oddCount += this.arr[i] % 2 != 0 ? 1 : 0;
         }
-        int odds[]=new int[oddCount];
+        int odds[] = new int[oddCount];
         int j = 0;
-        for(int i=0;i<this.length;i++)
-        {
-            if(this.arr[i] % 2 != 0)
-                odds[j++]=this.arr[i];
+        for (int i = 0; i < this.length; i++) {
+            if (this.arr[i] % 2 != 0)
+                odds[j++] = this.arr[i];
         }
         return odds;
     }
-    public int secondMax()
-    {
+
+    public int secondMax() {
         int max = Integer.MIN_VALUE;
         int secMax = Integer.MIN_VALUE;
-        for(int i=0;i<length;i++)
-        {
-            if(this.arr[i] > max)
-            {
-          
-                secMax=max;
+        for (int i = 0; i < length; i++) {
+            if (this.arr[i] > max) {
+
+                secMax = max;
                 max = this.arr[i];
-            }else if(this.arr[i] > secMax && this.arr[i] != max)
-            {
-                secMax=this.arr[i];
+            } else if (this.arr[i] > secMax && this.arr[i] != max) {
+                secMax = this.arr[i];
             }
         }
         return secMax;
     }
-    public void display(int a[])
-    {
+
+    public void display(int a[]) {
         System.out.print(Arrays.toString(a));
     }
-    public int[] moveZerotoEnd()
-    {
-        int temp[]=new int [length];
+
+    public int[] moveZerotoEnd() {
+        int temp[] = new int[length];
         int j = 0;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(this.arr[i] !=0)
-                temp[j++]=this.arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (this.arr[i] != 0)
+                temp[j++] = this.arr[i];
         }
-        for(int i=j;i<temp.length;i++)
-        {
-            temp[i]=0;
+        for (int i = j; i < temp.length; i++) {
+            temp[i] = 0;
         }
         return temp;
     }
+
     public int linearSearch(int element) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]==element) {
+            if (arr[i] == element) {
                 return i;
             }
         }
         return -1;
     }
+
     public int binarySearch(int element) {
         int low = 0;
-        int high = this.length-1;
+        int high = this.length - 1;
         while (low < high) {
-            int mid = (low+high)/2;
+            int mid = (low + high) / 2;
             if (arr[mid] == element) {
                 return mid;
-            }else if (arr[mid] < element) {
-                low = mid+1;
-            }else{
-                high=mid-1;
+            } else if (arr[mid] < element) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
     }
+
     public void bubbleSort() {
         // We take the largest elements to the last
         boolean isSwap;
-        for (int i = 0; i < arr.length-1; i++) {
-            isSwap=false;
-            for (int j = 0; j < arr.length-1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                    isSwap=true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            isSwap = false;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    isSwap = true;
                 }
             }
             if (!isSwap) {
@@ -123,14 +118,31 @@ public class ArraysImplementation {
             }
         }
     }
+
     public void insertionSort() {
         // We sort it like sorting a pack of cards in hand
-        // We take a card and consider as sorted then we pick another card and place it in a sorted order with another card
+        // We take a card and consider as sorted then we pick another card and place it
+        // in a sorted order with another card
+        //From sorted part we take first element and place it in soted array. this is done by shifitng al elements which are larger than first element by one position.
+        // It is repeates until unsorted array is finished 
+        // Elements on left are sorted and right are unsorted
+        // We always start from I=1
+        for (int i = 1; i < arr.length; i++) {
+            int temp=arr[i];
+            int j = i-1;
+            while (j>=0 && arr[j]>temp) {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
     }
+
     public static void main(String[] args) {
         ArraysImplementation array = new ArraysImplementation(5);
-        int pass[]={0,5,6, 0 , 8};
+        int pass[] = { 0, 5, 6, 0, 8 };
         array.setArray(pass);
         array.bubbleSort();
+        System.out.println("hello");
     }
 }
