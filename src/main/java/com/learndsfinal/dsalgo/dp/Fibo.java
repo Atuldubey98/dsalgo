6de7e53a35cce75a62702807d8b8d1d11ab1046e
Atul Dubey <atuldubey017@gmail.com>
2022-12-10 14:43:49 +0530

    add

--- src/main/java/com/learndsfinal/dsalgo/dp/Fibo.java
+++ src/main/java/com/learndsfinal/dsalgo/dp/Fibo.java
@@ -4,15 +4,15 @@ public class Fibo{
     // this is bottom up approach
     public static int fibo(int n)
     {
-	int table[]=new int[n+1];
-	table[0]=0;
-        
-	table[1]=1;
-	for(int i=2;i<n;i++)
-	{
-	    table[i]=table[i-1]+table[i-2];
-	}
-	return table[n];
+    	int table[]=new int[n+1];
+    	table[0]=0;
+            
+    	table[1]=1;
+    	for(int i=2;i<n;i++)
+    	{
+    	    table[i]=table[i-1]+table[i-2];
+    	}
+    	return table[n];
     }
     // this is top down approach
     public static int fibo(int memo[], int n)
